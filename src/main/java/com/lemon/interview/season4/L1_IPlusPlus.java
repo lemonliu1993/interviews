47fda43c04ce33b9eb2d14da581342c226755e24
lemonliu1993 <2305247091@qq.com>
2024-11-12 08:20:23 +0800

    init

---
+++ src/main/java/com/lemon/interview/season4/L1_IPlusPlus.java
@@ -0,0 +1,19 @@
+package com.lemon.interview.season4;
+
+/**
+ * Created by lemoon on 2024/11/11 09:01
+ */
+public class L1_IPlusPlus {
+    public static void main(String[] args) {
+        int i = 1;
+
+        System.out.println("i: "   + i);        //1
+        System.out.println("++i: " + ++i);      //2
+        System.out.println("i++: " + i++);      //2
+        System.out.println("i: "   + i);        //3
+        System.out.println("--i: " + --i);      //2
+        System.out.println("i--: " + i--);      //2
+        System.out.println("i: "   + i);        //1
+
+    }
+}
